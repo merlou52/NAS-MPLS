93847bff9785f48e69b46233167ed828039e45c0
Berthomieu Romain <rberthomie@tc405-114-07.insa-lyon.fr>
2021-12-08 11:42:29 +0100

    configs completed, json updated

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import telnetlib
+import json
+#import util
 from time import sleep
 
-r1 = telnetlib.Telnet("127.0.0.1", "5003")
-print("connected")
-print(r1.read_until(b"#"))
-r1.write(b"\r\n")
-r1.write(b'show ip route\r\n')
-print(r1.read_until(b"#"))
+f = open('config_routers.json')
+config = json.load(f)
+f.close()
+
+s = config["nb_routers"]
+print(f"nb_routers is {s}")
+
+
+for router in config[routers]:
+    r = telnetlib.Telnet(router["loopback"], router["port"])
+    terminal = util.Commands(r)
+    print("connected to "+router["name"])
+    print(r.read_until(b"#"))
+    r.write(b"\r\n")
+    for interface in r[interfaces]:
+        terminal.config_interface(interface["name"], interface["IP"], interface["loopback"])
+
 print("end")
