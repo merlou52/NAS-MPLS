47ebec702ab08e0e1332eb831a28123809e68707
Berthomieu Romain <rberthomie@tc405-114-02.insa-lyon.fr>
2021-12-17 11:53:08 +0100

    MAJ script OSPF : OK Interfaces : OK

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telnetlib
 import json
-#import util
+import util
 from time import sleep
 
 f = open('config_routers.json')
@@ -10,14 +10,18 @@ f.close()
 s = config["nb_routers"]
 print(f"nb_routers is {s}")
 
+#sleep(5)
 
-for router in config[routers]:
-    r = telnetlib.Telnet(router["loopback"], router["port"])
+for router in config["routers"]:
+    r = telnetlib.Telnet("localhost", router["port"])
     terminal = util.Commands(r)
     print("connected to "+router["name"])
-    print(r.read_until(b"#"))
-    r.write(b"\r\n")
-    for interface in r[interfaces]:
-        terminal.config_interface(interface["name"], interface["IP"], interface["loopback"])
+    r.read_until(b"#")
+    r.write(b"\r")
+    for interface in router["interfaces"]:
+        terminal.config_interface(interface["name"], interface["IP"], interface["netmask"])
+
+    terminal.config_OSPF(config, router)
+
 
 print("end")
--- util.py
+++ util.py
@@ -1,18 +1,53 @@
 import time
+import telnetlib
 
-class Commands
+class Commands:
 
-    def _init(self, tn):
+    def __init__(self, tn):
         self.tn = tn
 
     def command(self, string):
-        self.tn.write(f'{string}\r\n'.encode("utf-8"))
+        self.tn.write(f'{string}\r'.encode("utf-8"))
         #print(self.tn.read_until(b"#"))
-        sleep(0.1)
+        time.sleep(0.1)
 
-    def config_interface(self, name, ip, loopback):
-        r.command('configure terminal')
-        r.command(f'interface {name}')
-        r.command(f'ip address {ip} {loopback}')
-        r.command('no shutdown')
-        r.command('end')
+    def write(self):
+        self.command('write')
+        for i in range(10):
+            self.command('')
+            time.sleep(0.1)
+
+    def config_interface(self, name, ip, mask):
+        self.command('configure terminal')
+        self.command(f'interface {name}')
+        self.command(f'ip address {ip} {mask}')
+        self.command('no shutdown')
+        self.command('end')
+        self.write()
+
+    def config_OSPF(self, config, router):
+        self.command(f'configure terminal')
+        self.command(f'router ospf {config["process_ID"]}')
+        self.command(f'router-id {router["OSPF_ID"]}')
+        self.command(f'end')
+
+        for interface in router["interfaces"]:
+            if (interface["is_loopback"] == True):
+                self.command(f'configure terminal')
+                self.command(f'router ospf {config["process_ID"]}')
+                self.command(f'network {interface["addr_loopback"]} area 0')
+                self.command(f'end')
+
+            else:
+                if (interface["is_core"] == True):
+                    self.command(f'configure terminal')
+                    self.command(f'interface {interface["name"]}')
+                    self.command(f'ip ospf {config["process_ID"]} area 0')
+                    self.command(f'end')
+                else :
+                    self.command(f'configure terminal')
+                    self.command(f'router ospf {config["process_ID"]}')
+                    self.command(f'passive-interface {interface["name"]}')
+                    self.command(f'end')
+
+            self.write()
