3c169df2431ac3b7f1dd81aead682019cfab329a
gaspard <gaspard.michel@insa-lyon.fr>
2021-12-19 22:29:28 +0100

    Ajout commande par ligne

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import telnetlib
 import json
 import util
 from time import sleep
+import sys
 
 f = open('config_routers.json')
 config = json.load(f)
@@ -12,17 +13,35 @@ print(f"nb_routers is {s}")
 
 #sleep(5)
 
-for router in config["routers"]:
-    r = telnetlib.Telnet("localhost", router["port"])
-    terminal = util.Commands(r)
-    print("connected to "+router["name"])
-    r.read_until(b"#")
-    r.write(b"\r")
-    for interface in router["interfaces"]:
-        terminal.config_interface(interface["name"], interface["IP"], interface["netmask"])
+if len(sys.argv) < 2:
+    print("Pas le bon nombre d'argument !")
+    exit()
 
-    terminal.config_OSPF(config, router)
-    terminal.config_MPLS(config, router)
+if sys.argv[1] == '0':
+
+    for router in config["routers"]:
+        r = telnetlib.Telnet("localhost", router["port"])
+        terminal = util.Commands(r)
+        print("connected to "+router["name"])
+        r.read_until(b"#")
+        r.write(b"\r")
+        for interface in router["interfaces"]:
+            terminal.config_interface(interface["name"], interface["IP"], interface["netmask"])
+
+        terminal.config_OSPF(config, router)
+        terminal.config_MPLS(config, router)
+
+else :
+    if len(sys.argv) < 3:
+        print("<port> <commande>")
+        exit()
+    r = telnetlib.Telnet("localhost", sys.argv[1])
+    terminal = util.Commands(r)
+    com = ""
+    for i in range(len(sys.argv) - 2):
+        com = com + sys.argv[i+2] + " "
+    terminal.command(com)
+    print("On a pas encore implémenté")
 
 
 print("end")
