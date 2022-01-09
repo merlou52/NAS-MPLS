4ce0c0e7b69d8cd0542206498681594db45d3830
Antoine Merle <antoine.merle1@outlook.fr>
2022-01-10 00:37:06 +0100

    config gns3 manuelle fonctionnelle

--- util.py
+++ util.py
@@ -1,5 +1,5 @@
 import time
-import telnetlib
+
 
 class Commands:
 
@@ -8,12 +8,12 @@ class Commands:
 
     def command(self, string):
         self.tn.write(f'{string}\r'.encode("utf-8"))
-        #print(self.tn.read_until(b"#"))
-        time.sleep(0.1)
+        # print(self.tn.read_until(b"#"))
+        time.sleep(0.1) # pour permettre que le routeur soit pas submergé par les commandes
 
     def write(self):
         self.command('write')
-        for i in range(10):
+        for i in range(10): # pour etre sur que le routeur capte bien qu'on lui dise 'yes'
             self.command('')
             time.sleep(0.1)
 
@@ -52,7 +52,7 @@ class Commands:
                     self.command(f'interface {interface["name"]}')
                     self.command(f'ip ospf {config["process_ID"]} area 0')
                     self.command(f'end')
-                else :
+                else:
                     self.command(f'configure terminal')
                     self.command(f'router ospf {config["process_ID"]}')
                     self.command(f'passive-interface {interface["name"]}')
@@ -70,28 +70,28 @@ class Commands:
                 self.command(f'interface {interface["name"]}')
                 self.command('mpls ip')
                 self.command('exit')
-        
+
         self.command('end')
         self.write()
-    
+
     def config_BGP(self, config, router, num_router, nb_routers):
         self.command('configure terminal')
         self.command('router bgp 111')
         self.command('no sync')
         self.command(f'bgp router-id 1.1.1.{num_router}')
 
-        for i in (range(nb_routers) ):
+        for i in (range(nb_routers)):
             num = i + 1
             if i + 1 != num_router:
                 self.command(f'neighbor {num}.{num}.{num}.{num} remote-as 111')
                 self.command(f'neighbor {num}.{num}.{num}.{num} update-source Loopback0')
                 self.command(f'neighbor {num}.{num}.{num}.{num} activate')
                 self.command('network 1.1.1.0')
-        
+
         self.command('address-family ipv4')
 
         for interface in router["interfaces"]:
-            if (interface["is_core"] == False):
+            if not interface["is_core"]:
                 self.command(f'neighbor 1.{num_router}.0.2 remote-as 21{num_router}')
                 self.command(f'neighbor 1.{num_router}.0.2 activate')
 
